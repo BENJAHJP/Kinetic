b84b04c511fb9a6bc405f52521b88f860154c072
BENJAH <benjahjp@gmail.com>
2023-01-28 15:29:57 +0300

    fix:

--- app/src/main/java/com/example/kinetic/presentation/search/components/SearchScreen.kt
+++ app/src/main/java/com/example/kinetic/presentation/search/components/SearchScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
@@ -167,6 +168,7 @@ fun SearchScreen(
                     if (isNextLoading) {
                         CircularProgressIndicator(
                             modifier = Modifier.align(Alignment.BottomCenter)
+                                .padding(bottom = 50.dp)
                         )
                     }
                 }
